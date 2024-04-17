0d4faa9cc3a189ed8903ced29f11870e19078d7d
Qamar/dev <chaudhryqamar.17@gmail.com>
2024-04-17 21:14:31 +0500

    personal branch added qamarjsx/dev

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='h-screen bg-black grid place-items-center text-white text-5xl'>Home</div>
+    <div className='h-screen bg-black grid place-items-center text-white text-5xl'>Juus Home</div>
   )
 }
 
