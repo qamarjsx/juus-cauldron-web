a2d25068a50bb28e7ec7dd4c9592a42c9dcd7e0f
Qamar/dev <chaudhryqamar.17@gmail.com>
2024-05-06 19:22:43 +0500

    JuusHome is improved responsively

--- src/components/pages/JuusHome/JuusHome.jsx
+++ src/components/pages/JuusHome/JuusHome.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import JuusHeader from "../../utils/JuusHeader.jsx";
 import { Link } from "react-router-dom";
 import Footer from "../../utils/Footer.jsx";
 
 function JuusHome() {
-  const [arrowColor, setArrowColor] = useState("white");
 
   return (
     <div className="relative overflow-x-hidden bg-black scroll-smooth">
       <div className="">
         <JuusHeader />
       </div>
-      <main className="mt-24 h-screen flex flex-col items-center justify-between lg:h-auto">
-        <div className="h-1/2">
-          <video controls autoPlay loop playsInline className="w-full aspect-video">
+      <main className="mt-24 xl:mt-16 3xl:mt-8 h-screen flex flex-col items-center justify-between lg:h-auto">
+        <div className="">
+          <video controls autoPlay loop playsInline className="w-full aspect-video xl:h-full 3xl:h-screen 4xl:aspect-auto">
             <source
               src={
                 "https://juusstorage.blob.core.windows.net/website/videos/home/juus-fit-ad-desktop.mp4"
@@ -22,23 +21,21 @@ function JuusHome() {
             />
             Your browser does not support the video tag.
           </video>
-          <div>
+          <div className="">
             <img
-              className="mt-4 xl:mt-2 lg:mt-8 xl:scale-[.8] 3xl:scale-[.7] 5xl:scale-[.6] xl:-ml-20 3xl:-ml-40 5xl:-ml-72 pointer-events-none select-none"
+              className="mt-4 xl:mt-2 lg:mt-8 sm:scale-90 lg:scale-[.8] xl:scale-75 3xl:scale-[.65] lg:-ml-16 xl:-ml-28 3xl:-ml-48 5xl:-ml-64 6xl:-ml-80 pointer-events-none select-none"
               src="https://juusstorage.blob.core.windows.net/creatives/Nova Home/Future is Sugar (typography).png"
               alt=""
             />
             <img
-              className="scale-105 xl:scale-[.85] 3xl:scale-[.75] 5xl:scale-[.65] xl:-ml-20 3xl:-ml-40 5xl:-ml-72 xl:-mt-6 3xl:-mt-20 5xl:-mt-28 pointer-events-none select-none"
+              className="scale-105 sm:scale-95 lg:scale-[.85] xl:scale-[.8] 3xl:scale-[.7] lg:-ml-16 xl:-ml-28 3xl:-ml-48 5xl:-ml-64 6xl:-ml-80 lg:-mt-8 xl:-mt-12 3xl:-mt-20 5xl:-mt-28 pointer-events-none select-none"
               src="https://juusstorage.blob.core.windows.net/creatives/Nova%20Home/Free%20Paragraph%20(typography).png"
               alt=""
             />
           </div>
         </div>
-        <Link className="my-32 3xl:my-44 5xl:my-56" to={"/juus/shop"}>
+        <Link className="sm:my-20 lg:my-24 xl:my-32 3xl:my-44 5xl:my-52" to={"/juus/shop"}>
           <button
-            onMouseEnter={() => setArrowColor("black")}
-            onMouseLeave={() => setArrowColor("white")}
             className="cursor-pointer flex items-center font-bold justify-between p-2 w-40 lg:w-56 xl:w-72 h-9 lg:h-11 text-snow text-sm lg border-2 rounded-3xl hover:bg-snow hover:text-rich-black hover:shadow-primary-shadow hover:shadow-snow transition"
           >
             <span className="mx-auto">DISCOVER FLAVORS</span>
